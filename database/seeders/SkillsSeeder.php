b71fed833c06d90e297e1f2a80f4d596ced649a8
Khurramov <khurramov@MacBook-Pro-Khurramov.local>
2024-10-24 17:32:00 +0500

    chore(skills): add controller, model, factory, migration, and seeder for skills

---
+++ database/seeders/SkillsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Skills;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SkillsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Skills::factory()->count(10)->create();
+    }
+}
