9f7cb213a755050db4bb78dd80ab2ea4e2b00bf5
Khurramov <khurramov@MacBook-Pro-Khurramov.local>
2024-10-24 19:07:56 +0500

    chore(social-network-user): add controller, model, factory, migration, and seeder for social network user

---
+++ app/Http/Controllers/SocialNetworkUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SocialNetworkUser;
+use Illuminate\Http\Request;
+
+class SocialNetworkUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SocialNetworkUser $socialNetworkUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SocialNetworkUser $socialNetworkUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SocialNetworkUser $socialNetworkUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SocialNetworkUser $socialNetworkUser)
+    {
+        //
+    }
+}
