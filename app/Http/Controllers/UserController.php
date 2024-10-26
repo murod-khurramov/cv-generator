0d9ae7f2e37017e1e187b3ff42fac5344164f66a
Khurramov <khurramov@MacBook-Pro-Khurramov.local>
2024-10-26 18:44:13 +0500

    Add UserController, configure Sanctum, add personal access tokens migration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Illuminate\Database\Eloquent\Collection
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $user = User::factory()->create([
+            'first_name' => $request['first_name'],
+            'last_name' => $request['last_name'],
+            'nt_id' => $request['nt_id'],
+            'image' => $request['image'],
+            'phone' => $request['phone'],
+            'profession' => $request['profession'],
+            'biography' => $request['biography'],
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'status' => 'success',
+            'token' => $user->createToken($user->first_name)->plainTextToken,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(User::query()->findOrFail($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): \Illuminate\Http\JsonResponse
+    {
+        $user = User::query()->findOrFail($id);
+        $user->update([
+            'first_name' => $request['first_name'],
+            'last_name' => $request['last_name'],
+            'nt_id' => $request['nt_id'],
+            'image' => $request['image'],
+            'phone' => $request['phone'],
+            'profession' => $request['profession'],
+            'biography' => $request['biography'],
+        ]);
+
+        return response()->json([
+            'message' => 'User updated successfully',
+            'status' => 'success',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): \Illuminate\Http\JsonResponse
+    {
+        $user = User::query()->findOrFail($id);
+        $user->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully',
+            'status' => 'success',
+        ]);
+    }
+
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::resource('/users', UserController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('/projects', ProjectController::class)
+    ->middleware('auth:sanctum');
